ffdb029b4de4b968c5b95441373d412464479c2c
k <ksjk1998@gmail.com>
2018-11-09 18:15:37 -0500

    added writefile.lisp. Reads files and stores it into a list of str. Also creates /etc/default.cwp

--- writefile.lisp
+++ writefile.lisp
@@ -1,9 +1,16 @@
+(defun read-file (filename) 
+ (defvar lines '(filename))
+  (with-open-file (stream filename)
+    (do ((line (read-line stream nil)(read-line stream nil)))
+     ((null line))
+     (push line (cdr (last lines)))))
+  lines)
+
 (with-open-file (str "/etc/default.cwp"
                      :direction :output
                      :if-exists :supersede
                      :if-does-not-exist :create)
-  (with-open-file (stream "default.cwp")
-    (loop for line = (read-line stream nil)
-          while line
-          do (format str "~a~%" line))))
-(quit)
+  (loop for line in (cdr (read-file "default.cwp"))
+       do (format str "~a~%" line)))
+
+(print (read-file "default.cwp"))
