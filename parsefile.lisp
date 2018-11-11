8f4a6578c7061f9fd711f19009eac13764ade902
squealtv <squealtv@localhost.localdomain>
2018-11-11 15:23:57 -0500

    turned the proof-of-concept print statement into two functions

--- parsefile.lisp
+++ parsefile.lisp
@@ -1,3 +1,15 @@
 (print (read-file "default.cwp"))
-(print (split-sequence:SPLIT-SEQUENCE #\; (remove #\Space "one-more; and-another; and-another-rule; yet-another-rule; another-rule; rule;")))
+(defvar *whitespace* '(#\space #\newline #\backspace
+		       #\tab #\rubout #\linefeed
+		       #\page #\return))
+(defun split-semicolon (string)
+  "splits the string where it finds a semicolon, and strips the resulting strings of whitespace"
+  (let ((string-list ()))
+    (dolist (x (split-sequence:split-sequence #\; string))
+      (setq string-list (cons (string-trim *whitespace* x) string-list)))
+    string-list))
+(defun strip-empty-strings (string-list)
+  (remove-if #'(lambda (string) (string= string "")) string-list))
+(strip-empty-strings (split-semicolon
+		      "one-more; and-another; and-another-rule; yet-another-rule; another-rule; rule;"))
 (quit)
