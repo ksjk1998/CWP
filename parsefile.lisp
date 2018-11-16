291c4bb521aed9cc37c6dfb2f38db9e8869f7d1e
k <ksjk1998@gmail.com>
2018-11-16 15:00:17 -0500

    copied another dudes homework and now we can split colons. note: needs to split the last colon, not every colon in the string

--- parsefile.lisp
+++ parsefile.lisp
@@ -10,10 +10,24 @@
     (dolist (x (split-sequence:split-sequence #\; string))
       (setq string-list (cons (string-trim *whitespace* x) string-list)))
     (reverse string-list)))
+
+(defun split-colon (string)
+  "copied someone elses homework and changed one value, needs improvment"
+  (let ((string-list ()))
+    (dolist (x (split-sequence:split-sequence #\: string))
+      (setq string-list (cons (string-trim *whitespace* x) string-list)))
+    (reverse string-list)))
+
 (defun strip-empty-strings (string-list)
   (remove-if #'(lambda (string) (string= string "")) string-list))
-(print (let ((newlist nil))
-     (dolist (x (read-file "default.cwp"))
-            (push (strip-empty-strings (split-semicolon x)) newlist))
-          newlist))
+
+(print (let ((colon-list nil))
+     (dolist (y (let ((rule-list nil))
+                  (dolist (x (read-file "default.cwp"))
+                    (push (strip-empty-strings (split-semicolon x)) rule-list))
+                  (reverse rule-list)))
+            (dolist (z y)
+	      (push (strip-empty-strings (split-colon z)) colon-list)))
+          (reverse colon-list)))
+
 (quit)
