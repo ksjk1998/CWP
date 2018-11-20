22575fc0fdeeb3cd396223f5ae4f88cbc7d732ae
k <ksjk1998@gmail.com>
2018-11-19 20:28:17 -0500

    changed filenames

---
+++ write-file.lisp
@@ -0,0 +1,17 @@
+(defun read-file (filename) 
+ (let ((lines (cons filename nil))) 
+  (with-open-file (stream filename)
+    (do ((line (read-line stream nil)(read-line stream nil)))
+     ((null line))
+     (push line (cdr (last lines)))))
+  lines))
+
+; will be implemented in a way that runs for the first time you execute the server
+;(with-open-file (str "/etc/default.cwp"
+;                     :direction :output
+;                     :if-exists :supersede
+;                     :if-does-not-exist :create)
+;  (loop for line in (cdr (read-file "default.cwp"))
+;       do (format str "~a~%" line)))
+
+;(print (read-file "default.cwp"))
